ed6c92a97b10456154fe1b946656929cd0f4462d
Lengran <20275840+lengran@users.noreply.github.com>
2025-01-01 12:12:09 +0800

    feature: freeze time only, directly send

--- CS2DropKnife.cs
+++ CS2DropKnife.cs
@@ -5,6 +5,10 @@ using CounterStrikeSharp.API;
 using System.Numerics;
 using CounterStrikeSharp.API.Modules.Entities;
 using CounterStrikeSharp.API.Modules.Menu;
+using CounterStrikeSharp.API.Modules.Utils;
+using Microsoft.VisualBasic;
+using Microsoft.Extensions.Logging.Abstractions;
+using System.Runtime.InteropServices;
 
 namespace CS2DropKnife;
 
@@ -12,9 +16,12 @@ public class CS2DropKnife : BasePlugin
 {
     public override string ModuleName => "CS2 Drop Knife";
 
-    public override string ModuleVersion => "2.1.0";
+    public override string ModuleVersion => "3.0.0";
 
     private List<int> player_slot_ids = new List<int>();
+
+    private List<int> ct_players = new List<int>();
+    private List<int> t_players = new List<int>();
 
     public override void Load(bool hotReload)
     {
@@ -43,6 +50,16 @@ public class CS2DropKnife : BasePlugin
             }
 
             player_slot_ids.Add(player.Slot);
+
+            if (player.Team == CsTeam.CounterTerrorist)
+            {
+                ct_players.Add(player.Slot);
+            }
+            if (player.Team == CsTeam.Terrorist)
+            {
+                t_players.Add(player.Slot);
+            }
+            
         }
 
         return HookResult.Continue;
@@ -83,79 +100,119 @@ public class CS2DropKnife : BasePlugin
             return;
         }
 
+        // Only allow dropping knife at freeze time
+        var gameRules = Utilities.FindAllEntitiesByDesignerName<CCSGameRulesProxy>("cs_gamerules")?.FirstOrDefault()?.GameRules;
+        if (gameRules != null && !gameRules.FreezePeriod)
+        {
+            return;
+        }
+
+        // Find all teammates
+        List<int> teammates = new List<int>();
+        int self_slot = player.Slot;
+        if (player.Team == CsTeam.CounterTerrorist)
+        {
+            foreach (int teammate in ct_players)
+            {
+                if (teammate != self_slot)
+                {
+                    teammates.Add(teammate);
+                }
+            }
+        }
+        if (player.Team == CsTeam.Terrorist)
+        {
+            foreach (int teammate in t_players)
+            {
+                if (teammate != self_slot)
+                {
+                    teammates.Add(teammate);
+                }
+            }
+        }
+
         // Drop knives
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < teammates.Count; i++)
         {
             player.GiveNamedItem("weapon_knife");
+
+            // First find the held knife
+            var weapons = player.PlayerPawn.Value?.WeaponServices?.MyWeapons!;
+            if (weapons == null)
+            {
+                break;
+            }
+            int knife_index = -1;
+            foreach (var weapon in weapons)
+            {
+                if (weapon != null && weapon.IsValid && weapon.Value != null)
+                {
+                    if (weapon.Value.DesignerName.Contains("knife") || weapon.Value.DesignerName.Contains("bayonet"))
+                    {
+                        knife_index = (int)weapon.Value.Index;
+                        break;
+                    }
+                }
+            }
+
+            // Then drop and teleport
+            if (knife_index != -1)
+            {
+                player.ExecuteClientCommand("slot3");
+                player.DropActiveWeapon();
+
+                // Find the knife by index and teleport it
+                var knife = Utilities.GetEntityFromIndex<CBasePlayerWeapon>(knife_index);
+                if (knife != null && knife.IsValid)
+                {
+                    var teammate = Utilities.GetPlayerFromSlot(teammates[i]);
+                    if (teammate != null && teammate.IsValid  && !teammate.IsBot && !teammate.IsHLTV
+                    && teammate.PawnIsAlive && teammate.Pawn != null && teammate.Pawn.IsValid && teammate.Pawn.Value != null)
+                    {
+                        knife.Teleport(teammate.Pawn.Value.AbsOrigin);
+                    }
+                }
+            }
         }
 
         // No more chance to drop in this round
         player_slot_ids.Remove(player.Slot);
 
         return;
-
-        // var weapons = player.PlayerPawn.Value.WeaponServices?.MyWeapons;
-
-        // // Player might have no weapon.
-        // if (weapons == null) 
-        // {
-        //     return;
-        // }
-
-        // // Find the knife.
-        // foreach (var weapon in weapons)
-        // {
-        //     if (weapon != null && weapon.IsValid && weapon.Value != null && weapon.Value.IsValid)
-        //     {
-        //         if (weapon.Value.DesignerName.Contains("knife") || weapon.Value.DesignerName.Contains("bayonet"))
-        //         {
-        //             // Console.WriteLine("[CS2DropKnife] knife index = " + weapon.Index + ", entityindex = " + weapon.Value.Index + ", designer name = " + weapon.Value.DesignerName);
-        //             for (int i = 0; i < 5; i++)
-        //             {
-        //                 player.GiveNamedItem(weapon.Value.DesignerName);
-        //             }
-
-        //             player_slot_ids.Remove(player.Slot);
-
-        //             return;
-        //         }
-        //     }
-        // }
-
-        // player.PrintToChat("[CS2DropKnife] Can't find a knife on you. Get one and try again please.");
     }
 
 
-    [GameEventHandler]
-    public HookResult OnPlayerChat(EventPlayerChat @event, GameEventInfo info)
-    {
-        int player_slot = @event.Userid;
+    // Enable this for chat filtering (might cause performance issues)
+    // [GameEventHandler]
+    // public HookResult OnPlayerChat(EventPlayerChat @event, GameEventInfo info)
+    // {
+    //     int player_slot = @event.Userid;
 
-        try
-        {
-            CCSPlayerController player = Utilities.GetPlayerFromSlot(player_slot)!;
-            if (player == null || !player.IsValid || player.IsBot || player.IsHLTV)
-            {
-                return HookResult.Continue;
-            }
+    //     try
+    //     {
+    //         CCSPlayerController player = Utilities.GetPlayerFromSlot(player_slot)!;
+    //         if (player == null || !player.IsValid || player.IsBot || player.IsHLTV)
+    //         {
+    //             return HookResult.Continue;
+    //         }
 
-            string chat_message = @event.Text;
+    //         string chat_message = @event.Text;
 
-            if (chat_message.StartsWith("!drop") 
-            || chat_message.StartsWith("/drop")
-            || chat_message.StartsWith(".drop")
-            || chat_message.StartsWith("!takeknife")
-            || chat_message.StartsWith("/takeknife")
-            || chat_message.StartsWith(".takeknife"))
-            {
-                DropKnife(player);
-            }
-        }
-        catch (System.Exception)
-        {
-            return HookResult.Continue;
-        }
+    //         if (chat_message.StartsWith("!drop") 
+    //         || chat_message.StartsWith("/drop")
+    //         || chat_message.StartsWith(".drop")
+    //         || chat_message.StartsWith("!takeknife")
+    //         || chat_message.StartsWith("/takeknife")
+    //         || chat_message.StartsWith(".takeknife"))
+    //         {
+    //             DropKnife(player);
+    //         }
+    //     }
+    //     catch (System.Exception)
+    //     {
+    //         return HookResult.Continue;
+    //     }
 
-        return HookResult.Continue;
-    }
+    //     return HookResult.Continue;
+    // }
 }
